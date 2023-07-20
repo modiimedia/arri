2e54eacf2f04ac84e47f47d517772e09ee8d2649
Joshua Sosso <josh@modiimedia.com>
2023-07-20 14:06:11 -0500

    start setting up jsonschema to dart transformation

---
+++ packages/arri-plugin-dart/src/lib/arri-plugin-dart.spec.ts
@@ -0,0 +1,7 @@
+import { arriPluginDart } from "./arri-plugin-dart";
+
+describe("arriPluginDart", () => {
+    it("should work", () => {
+        expect(arriPluginDart()).toEqual("arri-plugin-dart");
+    });
+});
---
+++ packages/arri-plugin-dart/src/lib/arri-plugin-dart.ts
@@ -0,0 +1,3 @@
+export function arriPluginDart(): string {
+    return "arri-plugin-dart";
+}
