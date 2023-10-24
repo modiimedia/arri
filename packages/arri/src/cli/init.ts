eb38c49324008354d2fa88efbad94c730eb5849a
Josh Michaels <31548851+joshmossas@users.noreply.github.com>
2023-10-24 21:43:43 +0800

    finalize init command

--- packages/arri/src/cli/init.ts
+++ packages/arri/src/cli/init.ts
@@ -1,8 +1,10 @@
-import fs from "node:fs";
+import fs, { readFileSync, rmSync, writeFileSync } from "node:fs";
 import { defineCommand } from "citty";
 import consola, { createConsola } from "consola";
 import Degit from "degit";
 import path from "pathe";
+import prettier from "prettier";
+import { kebabCase } from "scule";
 
 const logger = createConsola();
 
@@ -38,13 +40,35 @@ export default defineCommand({
             );
             process.exit(1);
         }
-        logger.error("This feature is not yet available");
         const degit = Degit("https://github.com/modiimedia/arri-starters", {
             force: context.args.force,
         });
         await degit.clone(dir);
+        rmSync(path.resolve(dir, "pnpm-lock.yaml"));
+        const packageJson = JSON.parse(
+            readFileSync(path.resolve(dir, "package.json"), {
+                encoding: "utf-8",
+            }),
+        );
+        packageJson.name = kebabCase(context.args.dir);
+        packageJson.description = "An RPC server created with Arri RPC";
+        packageJson.private = true;
+        delete packageJson.license;
+        delete packageJson.main;
+        delete packageJson.author;
+        delete packageJson.tags;
+        writeFileSync(
+            path.resolve(dir, "package.json"),
+            await prettier.format(JSON.stringify(packageJson), {
+                parser: "json",
+                tabWidth: 4,
+                semi: true,
+                endOfLine: "lf",
+                trailingComma: "all",
+            }),
+        );
         consola.log(
-            `Project initialized in ${dir}. Cd into the directory and install dependencies to get started.`,
+            `Project initialized in ${context.args.dir}. To get started:\ncd ${context.args.dir}\nnpm install\nnpm run dev`,
         );
         process.exit(0);
     },
