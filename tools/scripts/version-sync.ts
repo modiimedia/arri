18a44842cc2cf67eba03922dbfbf2ae06a5bfb77
Joshua Sosso <josh@modiimedia.com>
2023-12-01 11:04:08 -0600

    fix version sync bug

--- tools/scripts/version-sync.ts
+++ tools/scripts/version-sync.ts
@@ -5,7 +5,9 @@ import { globby } from "globby";
 import prettier from "prettier";
 
 async function main() {
-    const prettierConfig = await require("../../.prettierrc");
+    const prettierConfig = JSON.parse(
+        readFileSync(".prettierrc", { encoding: "utf-8" }),
+    );
     const packageJson = JSON.parse(
         readFileSync("package.json", { encoding: "utf-8" }),
     );
