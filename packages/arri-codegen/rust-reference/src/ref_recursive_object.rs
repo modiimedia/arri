15ec4995ada4c32f7809a474c08d24641634ceb0
joshmossas <josh@modiimedia.com>
2024-03-21 23:20:57 -0500

    recursive object example

--- packages/arri-codegen/rust-reference/src/ref_recursive_object.rs
+++ packages/arri-codegen/rust-reference/src/ref_recursive_object.rs
@@ -1,5 +1,9 @@
+#![allow(dead_code, unused_assignments)]
+
 use arri_client::{serde_json, ArriModel};
 use std::str::FromStr;
+
+// IGNORE BEFORE //
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct BinaryTree {
@@ -40,10 +44,42 @@ impl ArriModel for BinaryTree {
     }
 
     fn to_json_string(&self) -> String {
-        todo!()
+        let mut _json_output_ = "{".to_string();
+        _json_output_.push_str("\"left\":");
+        match &self.left {
+            Some(left_val) => {
+                _json_output_.push_str(left_val.to_json_string().as_str());
+            }
+            _ => _json_output_.push_str("null"),
+        }
+        _json_output_.push_str(",\"right\":");
+        match &self.right {
+            Some(right_val) => {
+                _json_output_.push_str(right_val.to_json_string().as_str());
+            }
+            _ => _json_output_.push_str("null"),
+        }
+        _json_output_.push("}");
+        _json_output_
     }
 
     fn to_query_params_string(&self) -> String {
-        todo!()
+        let mut query_parts: Vec<String> = Vec::new();
+        match &self.left {
+            Some(left_val) => {
+                query_parts.push(format!("left={}", left_val.to_query_params_string()));
+            }
+            _ => {
+                query_parts.push("left=null".to_string());
+            }
+        }
+        match &self.right {
+            Some(right_val) => {
+                query_parts.push(format!("right={}", right_val.to_query_params_string()));
+            }
+            _ => {
+                query_parts.push("right=null".to_string());
+            }
+        }
     }
 }
