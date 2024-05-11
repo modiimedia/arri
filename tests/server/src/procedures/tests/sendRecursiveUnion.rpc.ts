74f2e20cb804952c1b11395cb9e6b332b47d77d8
Joshua Sosso <josh@modiimedia.com>
2024-05-11 00:19:48 -0500

    update to new syntax

--- tests/server/src/procedures/tests/sendRecursiveUnion.rpc.ts
+++ tests/server/src/procedures/tests/sendRecursiveUnion.rpc.ts
@@ -10,29 +10,25 @@ type RecursiveUnion =
     | { type: "TEXT"; data: string }
     | { type: "SHAPE"; data: { width: number; height: number; color: string } };
 
-const RecursiveUnion = a.recursive<RecursiveUnion>(
-    (self) =>
-        a.discriminator("type", {
-            CHILD: a.object({
-                data: self,
-            }),
-            CHILDREN: a.object({
-                data: a.array(self),
-            }),
-            TEXT: a.object({
-                data: a.string(),
-            }),
-            SHAPE: a.object({
-                data: a.object({
-                    width: a.float64(),
-                    height: a.float64(),
-                    color: a.string(),
-                }),
+const RecursiveUnion = a.recursive<RecursiveUnion>("RecursiveUnion", (self) =>
+    a.discriminator("type", {
+        CHILD: a.object({
+            data: self,
+        }),
+        CHILDREN: a.object({
+            data: a.array(self),
+        }),
+        TEXT: a.object({
+            data: a.string(),
+        }),
+        SHAPE: a.object({
+            data: a.object({
+                width: a.float64(),
+                height: a.float64(),
+                color: a.string(),
             }),
         }),
-    {
-        id: "RecursiveUnion",
-    },
+    }),
 );
 
 export default defineRpc({
