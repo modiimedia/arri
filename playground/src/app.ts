bf146e2fa4cabe631419abf2cb13fd029122372a
Joshua Sosso <josh@modiimedia.com>
2024-07-26 20:03:35 -0500

    clean up

--- playground/src/app.ts
+++ playground/src/app.ts
@@ -1,5 +1,11 @@
 import { a } from "@arrirpc/schema";
-import { ArriApp, defineRpc, defineService, handleCors } from "@arrirpc/server";
+import {
+    ArriApp,
+    defineEventStreamRpc,
+    defineRpc,
+    defineService,
+    handleCors,
+} from "@arrirpc/server";
 
 const app = new ArriApp({
     async onRequest(event) {
@@ -7,37 +13,6 @@ const app = new ArriApp({
             origin: "*",
         });
     },
-    onError(error) {
-        console.log({
-            name: error.name,
-            code: error.code,
-            message: error.message,
-            cause: error.cause,
-            stack: error.stack,
-        });
-    },
 });
-
-const usersService = defineService("users", {
-    getUser: defineRpc({
-        params: a.object("GetUserParams", {
-            userId: a.string(),
-        }),
-        response: a.object("User", {
-            id: a.string(),
-            name: a.string(),
-            createdAt: a.timestamp(),
-        }),
-        handler({ params }) {
-            return {
-                id: params.userId,
-                name: "",
-                createdAt: new Date(),
-            };
-        },
-    }),
-});
-
-app.use(usersService);
 
 export default app;
