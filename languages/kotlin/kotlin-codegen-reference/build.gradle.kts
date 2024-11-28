c6cb4f59ddcf24093c03a3e234ce2e832a09effc
Joshua Sosso <josh@modiimedia.com>
2024-11-28 13:26:21 -0600

    update kotlin dependencies

--- languages/kotlin/kotlin-codegen-reference/build.gradle.kts
+++ languages/kotlin/kotlin-codegen-reference/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
     application
 }
 
@@ -22,7 +22,7 @@ dependencies {
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-websockets:$ktorVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 }
 
 tasks.test {
