5384042b5876f5f61ba2e056e8dbba4e0be1d68b
Joshua Sosso <josh@modiimedia.com>
2025-01-11 19:15:39 -0600

    fix standard schema bug

--- languages/ts/ts-schema/src/compile.test.ts
+++ languages/ts/ts-schema/src/compile.test.ts
@@ -20,4 +20,24 @@ describe('standard-schema support', () => {
             assertType<User>(result.value);
         }
     });
+    it('produces the same result', async () => {
+        const input: User = {
+            id: '',
+            name: '',
+            email: null,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        };
+        expect($$User.validate(input)).toBe(true);
+        let standardResult = await $$User['~standard'].validate(input);
+        expect(typeof standardResult.issues).toBe('undefined');
+        if (!standardResult.issues) {
+            expect(standardResult.value).toStrictEqual(input);
+        }
+        delete (input as any).createdAt;
+        expect($$User.validate(input)).toBe(false);
+        standardResult = await $$User['~standard'].validate(input);
+        expect(standardResult.issues?.length).toBe(1);
+        expect(standardResult.issues?.[0]?.path).toStrictEqual(['createdAt']);
+    });
 });
--- languages/ts/ts-schema/src/standardSchema.ts
+++ languages/ts/ts-schema/src/standardSchema.ts
@@ -1,5 +1,6 @@
 import { StandardSchemaV1 } from '@standard-schema/spec';
 
+import { ValidationError } from './_index';
 import { ASchema, ValidationContext } from './schemas';
 
 export function createStandardSchemaProperty<T>(
@@ -36,6 +37,16 @@ export function createStandardSchemaProperty<T>(
                     value: result!,
                 };
             } catch (err) {
+                if (err instanceof ValidationError) {
+                    return {
+                        issues: err.errors.map((err) => ({
+                            message: err.message ?? 'Unknown error',
+                            path: err.instancePath
+                                .split('/')
+                                .filter((item) => item.length > 0),
+                        })),
+                    };
+                }
                 return {
                     issues: [
                         {
