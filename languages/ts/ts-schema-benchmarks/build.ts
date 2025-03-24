f1d637e28a762d600205e3d8265b579586564c5b
Joshua Sosso <josh@modiimedia.com>
2025-03-24 17:19:20 -0500

    add bundle size benchmarks

---
+++ languages/ts/ts-schema-benchmarks/build.ts
@@ -0,0 +1,40 @@
+import fs from 'node:fs';
+
+import unpluginTypia from '@ryoppippi/unplugin-typia/esbuild';
+import { build } from 'esbuild';
+
+const bundleSizeCases = [
+    'arktype',
+    'arri',
+    'typebox',
+    'typia',
+    'valibot',
+    'zod',
+];
+
+async function main() {
+    await build({
+        entryPoints: [
+            ...bundleSizeCases.map((val) => `src/bundle-size/${val}.ts`),
+        ],
+        outdir: 'dist/bundle-size',
+        bundle: true,
+        plugins: [unpluginTypia({})],
+    });
+
+    const files = bundleSizeCases.map(
+        (val) => [val, `dist/bundle-size/${val}.js`] as const,
+    );
+
+    const results: { name: string; fileSizeKb: number }[] = [];
+    for (const [name, file] of files) {
+        const stats = fs.statSync(file);
+        results.push({ name, fileSizeKb: stats.size * 0.001 });
+    }
+    results.sort((a, b) => (a.fileSizeKb <= b.fileSizeKb ? -1 : 1));
+    for (const result of results) {
+        console.info(`${result.name}: ${result.fileSizeKb.toLocaleString()}kb`);
+    }
+}
+
+main();
--- languages/ts/ts-schema/src/_index.ts
+++ languages/ts/ts-schema/src/_index.ts
@@ -1,6 +1,5 @@
-import * as a from './lib/_namespace';
 export * from './compile';
 export * from './errors';
 export * from './lib/_index';
+export * as a from './lib/_namespace';
 export * from './schemas';
-export { a };
