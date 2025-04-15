5feb3702540a3d26bf5518dbfbde5169d7e44559
joshmossas <josh@modiimedia.com>
2025-04-14 22:00:42 -0500

    Start setting up default request handler

--- languages/ts/ts-client/src/request.ts
+++ languages/ts/ts-client/src/request.ts
@@ -1,6 +1,13 @@
 import { serializeSmallString } from '@arrirpc/schema';
 import { EventSourcePlusOptions, type HttpMethod } from 'event-source-plus';
-import { $Fetch, FetchError, FetchOptions, ofetch } from 'ofetch';
+import {
+    $Fetch,
+    createFetch,
+    Fetch,
+    FetchError,
+    FetchOptions,
+    ofetch,
+} from 'ofetch';
 
 import { ArriErrorInstance, isArriError } from './errors';
 import { getHeaders } from './utils';
@@ -17,6 +24,137 @@ export interface ArriRequestOptions {
     onResponseError?: FetchOptions['onResponseError'];
     timeout?: FetchOptions['timeout'];
     signal?: FetchOptions['signal'];
+}
+
+export interface ArriRequest<TParams, TOutput> {
+    transport: string;
+    path: string;
+    clientVersion: string;
+    method?: string;
+    params?: TParams;
+    onError?: (err: unknown) => void | Promise<void>;
+    queryStringEncoder: (params: TParams) => string;
+    bodyEncoder: (params: TParams) => string;
+    responseDecoder: (input: unknown) => TOutput;
+    timeout?: number;
+}
+
+export interface ArriRequestHandler {
+    transport: string;
+    handleRequest<TParams, TOutput>(
+        req: ArriRequest<TParams, TOutput>,
+    ): Promise<TOutput> | TOutput;
+}
+
+export type HeaderFn = () =>
+    | Promise<Record<string, string>>
+    | Record<string, string>;
+
+export class HttpRequestHandler implements ArriRequestHandler {
+    transport: string = 'http';
+    baseUrl: string;
+    headers: Record<string, string> | HeaderFn;
+    ofetch: $Fetch;
+    retry?: FetchOptions['retry'];
+    retryDelay?: FetchOptions['retryDelay'];
+    retryStatusCodes?: FetchOptions['retryStatusCodes'];
+    onRequest?: FetchOptions['onRequest'];
+    onRequestError?: FetchOptions['onRequestError'];
+    onResponse?: FetchOptions['onResponse'];
+    onResponseError?: FetchOptions['onResponseError'];
+    timeout?: FetchOptions['timeout'];
+
+    constructor(options?: {
+        baseUrl?: string;
+        headers?: Record<string, string> | HeaderFn;
+        fetch?: Fetch;
+        retry?: FetchOptions['retry'];
+        retryDelay?: FetchOptions['retryDelay'];
+        retryStatusCodes?: FetchOptions['retryStatusCodes'];
+        onRequest?: FetchOptions['onRequest'];
+        onRequestError?: FetchOptions['onRequestError'];
+        onResponse?: FetchOptions['onResponse'];
+        onResponseError?: FetchOptions['onResponseError'];
+        timeout?: FetchOptions['timeout'];
+    }) {
+        this.baseUrl = options?.baseUrl ?? '';
+        this.headers = options?.headers ?? {};
+        this.ofetch = options?.fetch
+            ? createFetch({ fetch: options.fetch })
+            : ofetch;
+        this.retry = options?.retry;
+        this.retryDelay = options?.retryDelay;
+        this.retryStatusCodes = options?.retryStatusCodes;
+        this.onRequest = options?.onRequest;
+        this.onRequestError = options?.onRequestError;
+        this.onResponse = options?.onResponse;
+        this.onResponseError = options?.onResponseError;
+        this.timeout = options?.timeout;
+    }
+
+    async handleRequest<TParams, TOutput>(
+        req: ArriRequest<TParams, TOutput>,
+    ): Promise<TOutput> {
+        if (req.transport !== this.transport) {
+            throw new Error(
+                `HttpRequestHandler doesn't support the following transport: ${req.transport}`,
+            );
+        }
+        let url = this.baseUrl + req.path;
+        let body: undefined | string;
+        let contentType: undefined | string;
+        switch (req.method) {
+            case 'get':
+            case 'GET':
+            case 'head':
+            case 'HEAD':
+                if (req.params && typeof req.params === 'object') {
+                    url = `${url}?${req.queryStringEncoder(req.params)}`;
+                }
+                break;
+            default:
+                if (req.params && typeof req.params === 'object') {
+                    body = req.bodyEncoder(req.params);
+                    contentType = 'application/json';
+                }
+                break;
+        }
+        try {
+            const headers = (await getHeaders(this.headers)) ?? {};
+            if (contentType) headers['Content-Type'] = contentType;
+            if (req.clientVersion)
+                headers['client-version'] = req.clientVersion;
+            const result = await this.ofetch(url, {
+                method: req.method?.toUpperCase() ?? 'POST',
+                body,
+                headers,
+                timeout: this.timeout,
+                onRequest: this.onRequest,
+                onRequestError: this.onRequestError,
+                onResponse: this.onResponse,
+                onResponseError: this.onResponseError,
+            });
+            return req.responseDecoder(result);
+        } catch (err) {
+            const error = err as any as FetchError;
+            let arriError: ArriErrorInstance;
+            if (isArriError(error.data)) {
+                arriError = new ArriErrorInstance(error.data);
+            } else {
+                arriError = new ArriErrorInstance({
+                    code: error.statusCode ?? 500,
+                    message:
+                        error.statusMessage ??
+                        error.message ??
+                        `Error connecting to ${url}`,
+                    data: error.data,
+                    stack: error.stack,
+                });
+            }
+            if (req.onError) req.onError(arriError);
+            throw arriError;
+        }
+    }
 }
 
 export interface ArriRequestConfig<
