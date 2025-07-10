670ba91c9e426a0674cb9b8d73dc128c4ca11351
Joshua Sosso <josh@modiimedia.com>
2025-07-10 11:33:30 -0500

    better integration tests

--- internal/scripts/run-integration-tests.ts
+++ internal/scripts/run-integration-tests.ts
@@ -1,4 +1,5 @@
-import { execSync } from 'child_process';
+import { ofetch, waitFor } from '@arrirpc/client';
+import { ChildProcess, execSync, spawn } from 'child_process';
 import { defineCommand, runMain } from 'citty';
 import enquirer from 'enquirer';
 
@@ -33,7 +34,6 @@ const run = defineCommand({
         transport: {
             type: 'string',
             alias: 't',
-            default: 'http',
         },
     },
     async run({ args }) {
@@ -62,13 +62,19 @@ const run = defineCommand({
             ]);
             parsedArgs.transport = transport;
         }
-        let cmd = 'pnpm integration-tests-';
+        let buildServerCommand: string;
+        let startServerCommand: string;
+        let startServerCommandArgs: string[];
         switch (parsedArgs.server) {
             case 'go':
-                cmd += 'go';
+                buildServerCommand = `pnpm nx build-server test-server-go`;
+                startServerCommand = `pnpm`;
+                startServerCommandArgs = ['nx', 'start', 'test-server-go'];
                 break;
             case 'ts':
-                cmd += 'ts';
+                buildServerCommand = `pnpm nx build-server test-server-ts`;
+                startServerCommand = `pnpm`;
+                startServerCommandArgs = ['nx', 'start', 'test-server-ts'];
                 break;
             case undefined:
                 throw new Error(`Must specify type of server`);
@@ -76,11 +82,80 @@ const run = defineCommand({
                 parsedArgs.server satisfies never;
                 throw new Error(`Unsupported server type ${parsedArgs.server}`);
         }
+        let runTestsCommand: string;
         if (parsedArgs.affected) {
-            cmd += ':affected';
+            runTestsCommand = `pnpm nx affected -t integration-test`;
+        } else {
+            runTestsCommand = `pnpm nx run-many -t integration-test`;
         }
-        execSync(cmd, { stdio: 'inherit' });
+        if (parsedArgs.server === 'go') {
+            // swift client fails against go server for some reason
+            runTestsCommand += ` --exclude test-client-swift`;
+        }
+        if (parsedArgs.server === 'ts') {
+            // ts server sometime causes timeouts when running all the tests in parallel
+            runTestsCommand += ` --parallel=false`;
+        }
+        switch (parsedArgs.transport) {
+            case 'ws':
+                runTestsCommand += ` --configuration=websockets`;
+                break;
+            case 'http':
+            case undefined:
+                break;
+            default:
+                parsedArgs.transport satisfies never;
+                break;
+        }
+        execSync(buildServerCommand, { stdio: 'inherit' });
+        const childProcess = await launchDevServer(
+            startServerCommand,
+            startServerCommandArgs,
+            2020,
+        );
+        execSync(runTestsCommand, { stdio: 'inherit' });
+        childProcess.emit('close', 0);
+        process.exit(0);
     },
 });
 
 void runMain(run);
+
+async function launchDevServer(
+    cmd: string,
+    args: string[],
+    port: number,
+): Promise<ChildProcess> {
+    console.info(`starting dev server on port ${port}`);
+    const url = `http://127.0.0.1:${port}`;
+    const child = spawn(cmd, args);
+    return new Promise<ChildProcess>((res, rej) => {
+        let didConnect = false;
+        const timeout = setTimeout(() => {
+            if (didConnect) return;
+            rej(`Timeout exceeded waiting for dev server`);
+        }, 10000);
+        waitForGetRequest(url)
+            .then((result) => {
+                didConnect = result;
+                console.info(`dev server started at ${url}`);
+                clearTimeout(timeout);
+                res(child);
+            })
+            .catch((err) => rej(err));
+    });
+}
+
+async function waitForGetRequest(url: string) {
+    try {
+        const result = await ofetch.raw(url);
+        if (result.status >= 200 && result.status < 300) {
+            return true;
+        }
+        await waitFor(100);
+        return waitForGetRequest(url);
+    } catch (_) {
+        await waitFor(100);
+        return waitForGetRequest(url);
+    }
+}
