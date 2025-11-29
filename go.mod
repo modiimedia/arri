4acf594b7ee09a074c364350923227c83f812596
joshmossas <josh@modiimedia.com>
2025-11-29 15:15:05 -0600

    working on go stuff

--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/modiimedia/arri
 go 1.23.3
 
 require (
-	github.com/tidwall/gjson v1.18.0
 	github.com/coder/websocket v1.8.13
 	github.com/iancoleman/strcase v0.3.0
+	github.com/tidwall/gjson v1.18.0
+	github.com/lxzan/gws v1.8.9
 )
 
 require (
+	github.com/dolthub/maphash v0.1.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/rs/cors v1.11.1 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
--- languages/go/go-server/adapter_ws.go
+++ languages/go/go-server/adapter_ws.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
-	"github.com/coder/websocket"
+	"github.com/lxzan/gws"
 )
 
 type WsAdapter[T any] struct {
@@ -13,11 +14,38 @@ type WsAdapter[T any] struct {
 	globalOptions AppOptions[T]
 	httpRegister  HttpTransportAdapter[T]
 	options       WsAdapterOptions[T]
+	handlers      map[string]func(Message) ([]byte, RpcError)
 }
 
 type WsAdapterOptions[T any] struct {
 	OnUpgrade      func(req *Request[T], connection any) // TODO: type the ws connection
 	ConnectionPath string                                // defaults to "/ws"
+}
+
+func (w WsAdapter[T]) OnOpen(socket *gws.Conn) {
+	_ = socket.SetDeadline(time.Now().Add(w.globalOptions.HeartbeatInterval + 10*time.Second))
+}
+
+func (w WsAdapter[T]) OnClose(socket *gws.Conn, err error) {
+	// TODO
+}
+
+func (w WsAdapter[T]) OnPing(socket *gws.Conn, payload []byte) {
+	_ = socket.SetDeadline(time.Now().Add(w.globalOptions.HeartbeatInterval + 10*time.Second))
+	_ = socket.WritePong(nil)
+}
+
+func (w WsAdapter[T]) OnPong(socket *gws.Conn, payload []byte) {}
+
+func (w WsAdapter[T]) OnMessage(socket *gws.Conn, message *gws.Message) {
+	defer message.Close()
+	msg, err := DecodeMessage(message.Bytes())
+	if err != nil {
+		errBody, _ := EncodeJSON(err, EncodingOptions{KeyCasing: w.globalOptions.KeyCasing, MaxDepth: w.globalOptions.MaxDepth})
+		socket.WriteAsync(gws.OpcodeText, errBody, func(err error) {})
+		return
+	}
+	// TODO
 }
 
 func NewWsAdapter[T any](httpAdapter HttpTransportAdapter[T], options WsAdapterOptions[T]) *WsAdapter[T] {
@@ -26,6 +54,7 @@ func NewWsAdapter[T any](httpAdapter HttpTransportAdapter[T], options WsAdapterO
 		middlewares:  [](func(req *Request[T]) RpcError){},
 		options:      options,
 	}
+	setupWsConnectionHandler(adapter)
 	return adapter
 }
 
@@ -39,15 +68,17 @@ func setupWsConnectionHandler[T any](ws *WsAdapter[T]) {
 		MaxDepth:  ws.globalOptions.MaxDepth,
 	}
 	ws.httpRegister.RegisterEndpoint(connectionPath, func(w http.ResponseWriter, r *http.Request) {
-		c, err := websocket.Accept(w, r, nil)
+		upgrader := gws.NewUpgrader(ws, &gws.ServerOption{})
+		c, err := upgrader.Upgrade(w, r)
 		if err != nil {
 			payload, _ := EncodeJSON(ErrorWithData(400, err.Error(), Some[any](err)), encodingOptions)
 			w.WriteHeader(400)
 			w.Write(payload)
 			return
 		}
-		defer c.CloseNow()
-		// TODO: actually implement WS stuff
+		go func() {
+			c.ReadLoop()
+		}()
 	})
 }
 
@@ -55,11 +86,30 @@ func (_ WsAdapter[T]) TransportId() string {
 	return "ws"
 }
 
-func (wa *WsAdapter[T]) RegisterRpc(name string, def RpcDef, paramValidator Validator, responseValidator Validator, handler func(any, Request[T]) (any, RpcError)) {
-	// TODO
+func (w *WsAdapter[T]) RegisterRpc(name string, def RpcDef, paramValidator Validator, responseValidator Validator, handler func(any, Request[T]) (any, RpcError)) {
+	w.handlers[name] = func(msg Message) ([]byte, RpcError) {
+		if msg.Type != InvocationMessage {
+			return []byte{}, Error(400, fmt.Sprintf("Expected RPC invocation message. Got %s", msg.Type))
+		}
+		ctx := context.Background()
+		params, resErr := paramValidator.DecodeJSON(msg.Body.UnwrapOr([]byte{}))
+		if resErr != nil {
+			return []byte{}, resErr
+		}
+		req := NewRequest[T](ctx, name, w.TransportId(), "", "", map[string]string{})
+		res, resErr := handler(params, *req)
+		if resErr != nil {
+			return []byte{}, resErr
+		}
+		body, err := responseValidator.EncodeJSON(res)
+		if err != nil {
+			return []byte{}, Error(500, fmt.Sprintf("Error serializing response, %s", err))
+		}
+		return body, nil
+	}
 }
 
-func (wa *WsAdapter[T]) RegisterOutputStreamRpc(
+func (w *WsAdapter[T]) RegisterOutputStreamRpc(
 	name string,
 	def RpcDef,
 	paramValidator Validator,
@@ -69,35 +119,35 @@ func (wa *WsAdapter[T]) RegisterOutputStreamRpc(
 	// TODO
 }
 
-func (ws *WsAdapter[T]) SetGlobalOptions(options AppOptions[T]) {
-	ws.globalOptions = options
+func (w *WsAdapter[T]) SetGlobalOptions(options AppOptions[T]) {
+	w.globalOptions = options
 }
 
-func (ws *WsAdapter[T]) Use(middleware func(req *Request[T]) RpcError) {
-	ws.middlewares = append(ws.middlewares, middleware)
+func (w *WsAdapter[T]) Use(middleware func(req *Request[T]) RpcError) {
+	w.middlewares = append(w.middlewares, middleware)
 }
 
-func (ws WsAdapter[T]) Start() error {
-	connectionPath := ws.options.ConnectionPath
+func (w WsAdapter[T]) Start() error {
+	connectionPath := w.options.ConnectionPath
 	if len(connectionPath) == 0 {
 		connectionPath = "/ws"
 	}
-	if !ws.httpRegister.HasStarted() {
+	if !w.httpRegister.HasStarted() {
 		fmt.Printf("WS Connection path: %s\n", connectionPath)
-		return ws.httpRegister.Start()
+		return w.httpRegister.Start()
 	}
 
 	fmt.Printf("WS Connection path: %s\n", connectionPath)
 	return nil
 }
 
-func (ws WsAdapter[T]) HasStarted() bool {
-	return ws.httpRegister.HasStarted()
+func (w WsAdapter[T]) HasStarted() bool {
+	return w.httpRegister.HasStarted()
 }
 
-func (ws WsAdapter[T]) Close(ctx context.Context) error {
-	if ws.httpRegister.HasStarted() {
-		err := ws.httpRegister.Close(ctx)
+func (w WsAdapter[T]) Close(ctx context.Context) error {
+	if w.httpRegister.HasStarted() {
+		err := w.httpRegister.Close(ctx)
 		if err != nil {
 			return err
 		}
