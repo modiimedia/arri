d5dec71ecf3833dab444c38e1e0a221398e37e22
joshmossas <josh@modiimedia.com>
2026-09-08 04:34:43 -0500

    remove log

--- tests/servers/ts/src/procedures/tests/sendObject.rpc.ts
+++ tests/servers/ts/src/procedures/tests/sendObject.rpc.ts
@@ -71,7 +71,6 @@ export default defineRpc({
     params: ObjectWithEveryType,
     response: ObjectWithEveryType,
     handler({ params }) {
-        console.log('INPUT', params);
         return params;
     },
 });
